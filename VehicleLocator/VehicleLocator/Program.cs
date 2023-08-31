7e84c577615b651f24a1d76f1c462c6d2f1abd54
Onke <onke.lokothwayo@gmail.com>
2023-08-31 12:52:34 +0200

    First commit

---
+++ VehicleLocator/VehicleLocator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace VehicleLocator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "MIX Telematics Vehicle Locator" ;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Welcome to the Mix Vehicle Locator!");
+
+            /*
+             * Store file in reader. Start with first 10 results then once code works, read entire file.  
+             * Store co-ordinates in file and read them into a List.  
+             * 
+             */
+
+            
+        }
+
+        private void ReadVehiclePositions()
+        {
+
+        }
+
+        private void ReadCoOrdinates()
+        {
+
+        }
+
+        private void GetClosestVehicleToCoOrdinate()
+        {
+
+        }
+
+        private void DisplayResults()
+        {
+
+        }
+    }
+}
